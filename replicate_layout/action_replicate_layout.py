c68ac98a91b3b9631988086f14490c39c1356586
Mitja Nemec <mitja.nemec@fe.uni-lj.si>
2018-02-17 13:22:24 +0100

    Changed radio button implementation

--- replicate_layout/action_replicate_layout.py
+++ replicate_layout/action_replicate_layout.py
@@ -29,114 +29,119 @@ ___version___ = "1.0"
 class ReplicateLayoutDialog(wx.Dialog):
     def __init__(self, parent, radius, width, angle):
 
+        wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"Replicate layout", pos = wx.DefaultPosition, size = wx.Size( 270,387 ), style = wx.DEFAULT_DIALOG_STYLE )
+
+        bSizer1 = wx.BoxSizer( wx.VERTICAL )
+
+        bSizer9 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.rad_btn_Linear = wx.RadioButton( self, wx.ID_ANY, u"Linear", wx.DefaultPosition, wx.DefaultSize, wx.RB_GROUP )
+        self.rad_btn_Linear.SetValue(True)
+        bSizer9.Add( self.rad_btn_Linear, 0, wx.ALL, 5 )
+
+        self.rad_btn_Circular = wx.RadioButton( self, wx.ID_ANY, u"Circular", wx.DefaultPosition, wx.DefaultSize, wx.RB_GROUP )
+        self.rad_btn_Circular.SetValue(False)
+        bSizer9.Add( self.rad_btn_Circular, 0, wx.ALL, 5 )
+
+        bSizer1.Add( bSizer9, 1, wx.EXPAND, 5 )
+
+        bSizer2 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.lbl_x_mag = wx.StaticText( self, wx.ID_ANY, u"x offset (mm)", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.lbl_x_mag.Wrap( -1 )
+        bSizer2.Add( self.lbl_x_mag, 0, wx.ALL, 5 )
+
+        self.val_x_mag = wx.TextCtrl( self, wx.ID_ANY, u"0.0", wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer2.Add( self.val_x_mag, 0, wx.ALL, 5 )
+
+        bSizer1.Add( bSizer2, 1, wx.EXPAND, 5 )
+
+        bSizer3 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.lbl_y_angle = wx.StaticText( self, wx.ID_ANY, u"y offset (mm)", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.lbl_y_angle.Wrap( -1 )
+        bSizer3.Add( self.lbl_y_angle, 0, wx.ALL, 5 )
+
+        self.val_y_angle = wx.TextCtrl( self, wx.ID_ANY, u"0.0", wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer3.Add( self.val_y_angle, 0, wx.ALL, 5 )
+
+        bSizer1.Add( bSizer3, 1, wx.EXPAND, 5 )
+
+        bSizer8 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.chkbox_tracks = wx.CheckBox( self, wx.ID_ANY, u"Replicate tracks", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.chkbox_tracks.SetValue(True)
+        bSizer8.Add( self.chkbox_tracks, 0, wx.ALL, 5 )
+
+        bSizer1.Add( bSizer8, 1, wx.EXPAND, 5 )
+
+        bSizer5 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.chkbox_zones = wx.CheckBox( self, wx.ID_ANY, u"Replicate zones", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.chkbox_zones.SetValue(True)
+        bSizer5.Add( self.chkbox_zones, 0, wx.ALL, 5 )
+
+        bSizer1.Add( bSizer5, 1, wx.EXPAND, 5 )
+
+        bSizer10 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.chkbox_intersecting = wx.CheckBox( self, wx.ID_ANY, u"Replicate intersecting tracks/zones", wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer10.Add( self.chkbox_intersecting, 0, wx.ALL, 5 )
+
+        bSizer1.Add( bSizer10, 1, wx.EXPAND, 5 )
+
+        bSizer11 = wx.BoxSizer( wx.VERTICAL )
+
+        self.chkbox_remove = wx.CheckBox( self, wx.ID_ANY, u"Remove existing tracks/zones", wx.DefaultPosition, wx.DefaultSize, 0 )
+
+        bSizer11.Add( self.chkbox_remove, 0, wx.ALL, 5 )
+
+        bSizer1.Add( bSizer11, 1, wx.EXPAND, 5 )
+
+        bSizer12 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.btn_ok = wx.Button( self, wx.ID_OK, u"OK", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.btn_ok.SetDefault()
+        bSizer12.Add( self.btn_ok, 0, wx.ALL, 5 )
+
+        self.btn_cancel = wx.Button( self, wx.ID_CANCEL, u"Cancel", wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer12.Add( self.btn_cancel, 0, wx.ALL, 5 )
+
+        bSizer1.Add( bSizer12, 1, wx.EXPAND, 5 )
+
+        self.SetSizer( bSizer1 )
+        self.Layout()
+
+        self.Centre( wx.BOTH )
+
+        # Connect Events
+        self.rad_btn_Linear.Bind(wx.EVT_RADIOBUTTON, self.coordinate_system_changed)
+        self.rad_btn_Circular.Bind(wx.EVT_RADIOBUTTON, self.coordinate_system_changed)
+
         self.minimum_radius = radius
         self.minimum_width = width
         self.minimum_angle = angle
 
-        wx.Dialog.__init__(self, parent, id=wx.ID_ANY, title=u"Replicate layout", pos=wx.DefaultPosition,
-                           size=wx.Size(270, 387), style=wx.DEFAULT_DIALOG_STYLE)
-
-        #self.SetSizeHints(wx.Size(-1, -1), wx.Size(-1, -1))
-
-        bSizer1 = wx.BoxSizer(wx.VERTICAL)
-
-        bSizer9 = wx.BoxSizer(wx.HORIZONTAL)
-
-        coordinate_systemChoices = [u"Linear", u"Circular"]
-        self.coordinate_system = wx.RadioBox(self, wx.ID_ANY, u"Replication", wx.DefaultPosition,
-                                             wx.Size(250, -1), coordinate_systemChoices, 2, wx.RA_SPECIFY_COLS)
-        self.coordinate_system.SetSelection(0)
-        bSizer9.Add(self.coordinate_system, 0, wx.ALL, 5)
-
-        bSizer1.Add(bSizer9, 1, wx.EXPAND, 5)
-
-        bSizer2 = wx.BoxSizer(wx.HORIZONTAL)
-
-        self.lbl_x_mag = wx.StaticText(self, wx.ID_ANY, u"x offset (mm)", wx.DefaultPosition, wx.DefaultSize, 0)
-        self.lbl_x_mag.Wrap(-1)
-        bSizer2.Add(self.lbl_x_mag, 0, wx.ALL, 5)
-
-        self.val_x_mag = wx.TextCtrl(self, wx.ID_ANY, str(self.minimum_width), wx.DefaultPosition, wx.DefaultSize, 0)
-        bSizer2.Add(self.val_x_mag, 0, wx.ALL, 5)
-
-        bSizer1.Add(bSizer2, 1, wx.EXPAND, 5)
-
-        bSizer3 = wx.BoxSizer(wx.HORIZONTAL)
-
-        self.lbl_y_angle = wx.StaticText(self, wx.ID_ANY, u"y offset (mm)", wx.DefaultPosition, wx.DefaultSize, 0)
-        self.lbl_y_angle.Wrap(-1)
-        bSizer3.Add(self.lbl_y_angle, 0, wx.ALL, 5)
-
-        self.val_y_angle = wx.TextCtrl(self, wx.ID_ANY, u"0.0", wx.DefaultPosition, wx.DefaultSize, 0)
-        bSizer3.Add(self.val_y_angle, 0, wx.ALL, 5)
-
-        bSizer1.Add(bSizer3, 1, wx.EXPAND, 5)
-
-        bSizer8 = wx.BoxSizer(wx.HORIZONTAL)
-
-        self.chkbox_tracks = wx.CheckBox(self, wx.ID_ANY, u"Replicate tracks", wx.DefaultPosition, wx.DefaultSize,
-                                         0)
-        self.chkbox_tracks.SetValue(True)
-        bSizer8.Add(self.chkbox_tracks, 0, wx.ALL, 5)
-
-        bSizer1.Add(bSizer8, 1, wx.EXPAND, 5)
-
-        bSizer5 = wx.BoxSizer(wx.HORIZONTAL)
-
-        self.chkbox_zones = wx.CheckBox(self, wx.ID_ANY, u"Replicate zones", wx.DefaultPosition, wx.DefaultSize, 0)
-        self.chkbox_zones.SetValue(True)
-        bSizer5.Add(self.chkbox_zones, 0, wx.ALL, 5)
-
-        bSizer1.Add(bSizer5, 1, wx.EXPAND, 5)
-
-        bSizer10 = wx.BoxSizer(wx.HORIZONTAL)
-
-        self.chkbox_intersecting = wx.CheckBox(self, wx.ID_ANY, u"Replicate intersecting tracks/zones",
-                                               wx.DefaultPosition, wx.DefaultSize, 0)
-        bSizer10.Add(self.chkbox_intersecting, 0, wx.ALL, 5)
-
-        bSizer1.Add(bSizer10, 1, wx.EXPAND, 5)
-
-        bSizer11 = wx.BoxSizer(wx.VERTICAL)
-
-        self.chkbox_remove = wx.CheckBox(self, wx.ID_ANY, u"Remove existing tracks/zones", wx.DefaultPosition,
-                                         wx.DefaultSize, 0)
-
-        bSizer11.Add(self.chkbox_remove, 0, wx.ALL, 5)
-
-        bSizer1.Add(bSizer11, 1, wx.EXPAND, 5)
-
-        bSizer12 = wx.BoxSizer(wx.HORIZONTAL)
-
-        self.btn_ok = wx.Button(self, wx.ID_OK, u"OK", wx.DefaultPosition, wx.DefaultSize, 0)
-        self.btn_ok.SetDefault()
-        bSizer12.Add(self.btn_ok, 0, wx.ALL, 5)
-
-        self.btn_cancel = wx.Button(self, wx.ID_CANCEL, u"Cancel", wx.DefaultPosition, wx.DefaultSize, 0)
-        bSizer12.Add(self.btn_cancel, 0, wx.ALL, 5)
-
-        bSizer1.Add(bSizer12, 1, wx.EXPAND, 5)
-
-        self.SetSizer(bSizer1)
-        self.Layout()
-
-        self.Centre(wx.BOTH)
-
-        # Connect Events
-        self.coordinate_system.Bind(wx.EVT_RADIOBOX, self.coordinate_system_changed)
+        self.val_x_mag.SetValue("%.2f" % self.minimum_width)
+        self.val_y_angle.SetValue(u"0.0")
 
     def coordinate_system_changed(self, event):
+        rb = event.GetEventObject() 
         # if cartesian
-        if self.coordinate_system.GetSelection() == 0:
+        if rb.GetLabel() == u"Linear":
+            self.rad_btn_Linear.SetValue(True)
+            self.rad_btn_Circular.SetValue(False)
             self.lbl_x_mag.SetLabelText(u"x offset (mm)")
             self.lbl_y_angle.SetLabelText(u"y offset (mm)")
-            self.val_x_mag.SetValue(str(self.minimum_width))
+            self.val_x_mag.SetValue("%.2f" % self.minimum_width)
             self.val_y_angle.SetValue(u"0.0")
         else:
+            self.rad_btn_Linear.SetValue(False)
+            self.rad_btn_Circular.SetValue(True)
             self.lbl_x_mag.SetLabelText(u"radius (mm)")
             self.lbl_y_angle.SetLabelText(u"angle (deg)")
-            self.val_x_mag.SetValue(str(self.minimum_radius))
-            self.val_y_angle.SetValue(str(self.minimum_angle))
+            self.val_x_mag.SetValue("%.2f" % self.minimum_radius)
+            self.val_y_angle.SetValue("%.2f" % self.minimum_angle)
         pass
 
 
@@ -151,13 +156,6 @@ class ReplicateLayout(pcbnew.ActionPlugin):
     """
 
     def defaults(self):
-        """
-        Method defaults must be redefined
-        self.name should be the menu label to use
-        self.category should be the category (not yet used)
-        self.description should be a comprehensive description
-          of the plugin
-        """
         self.name = "Replicate layout"
         self.category = "Modify Drawing PCB"
         self.description = "Replicate layout of a hierchical sheet"
@@ -215,12 +213,12 @@ class ReplicateLayout(pcbnew.ActionPlugin):
             else:
                 process_canceled = True
 
-            if process_canceled == False:
+            if not process_canceled:
                 # execute replicate_layout
-                if (x_offset != None) and (y_offset != None):
+                if (x_offset is not None) and (y_offset is not None):
                     # are we replicating in polar coordinate system
                     polar = False
-                    if dlg.coordinate_system.GetSelection() != 0:
+                    if dlg.rad_btn_Circular.GetValue():
                         polar = True
 
                     # replicate now
