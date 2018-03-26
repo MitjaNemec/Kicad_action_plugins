b428f9ccd00970c293fe993b7d8af07db63b012f
Mitja Nemec <mitja.nemec@fe.uni-lj.si>
2018-03-26 21:50:31 +0200

    Added a 3D module archiving and relinking. Relinking has to be tested how it behaves if kicad_pcb is modified outside of pcbnew, when the file is already opened.

--- archive_schematics/archive_project.py
+++ archive_schematics/archive_project.py
@@ -1,6 +1,7 @@
 import pcbnew
 import os
 import os.path
+import shutil
 
 
 def archive_symbols(board, alt_files=False):
@@ -9,8 +10,8 @@ def archive_symbols(board, alt_files=False):
     cache_lib_name = project_name + "-cache.lib"
     
     # load system symbol library table
-    #sys_path = "C://Users//MitjaN//AppData//Roaming//kicad"
-    sys_path = pcbnew.GetKicadConfigPath()
+    sys_path = "C://Users//MitjaN//AppData//Roaming//kicad"
+    #sys_path = pcbnew.GetKicadConfigPath()
     global_sym_lib_file_path = sys_path + "//sym-lib-table"
     with open(global_sym_lib_file_path) as f:
         global_sym_lib_file = f.readlines()
@@ -45,7 +46,7 @@ def archive_symbols(board, alt_files=False):
     
     # if cache library is not on the list, put it there
     if cache_lib_name not in unicode(project_sym_lib_file):
-        line = "(lib (name cache)(type Legacy)(uri ${KIPRJ_MODE}//" + cache_lib_name + ")(options "")(descr ""))"
+        line = "(lib (name cache)(type Legacy)(uri ${KIPRJMOD}//" + cache_lib_name + ")(options "")(descr ""))"
         project_sym_lib_file.insert(1, line)
     
     # load cache library
@@ -97,9 +98,76 @@ def archive_symbols(board, alt_files=False):
     pass
 
 
+def archive_3D_models(board):
+    # load layout
+    filename = board.GetFileName()
+    with open(filename) as f:
+        pcb_layout = f.readlines()
+
+    # find all used models
+    models = []
+    for line in pcb_layout:
+        index = line.find("(model")
+        if index != -1:
+            line_split = line.split()
+            index = line_split.index("(model")
+            model_path = line_split[index+1]
+            model = os.path.basename(model_path)
+            models.append(model.rsplit(".", 1)[0])
+    models = list(set(models))
+
+    model_library_path = os.getenv("KISYS3DMOD")
+    # D:\Mitja\Plate\Kicad_libs\official_libs\Packages3D
+    model_library_path = "D://Mitja//Plate//Kicad_libs//official_libs//Packages3D"
+
+    # prepare folder for 3dmodels
+    proj_path = os.path.dirname(os.path.abspath(board.GetFileName()))
+
+    model_folder_path = proj_path + "//shapes3D"
+
+    if not os.path.exists(model_folder_path):
+        os.makedirs(model_folder_path)
+
+    # go through the list of used model
+    for model in models:
+        for root, directories, filenames in os.walk(model_library_path):
+            for filename in filenames:
+                if model in filename and (filename.endswith(".wrl") or filename.endswith(".stp") or filename.endswith(".step")):
+                    # copy file to model_folder_pah
+                    shutil.copy2(root+"//"+filename, model_folder_path)
+
+    # generate output file with project relative path
+    out_file = []
+    for line in pcb_layout:
+        line_new = line
+        index = line.find("(model")
+        if index != -1:
+            line_split = line.split("(model")
+            for model in models:
+                if model in line_split[1]:
+                    model_path = " ${KIPRJMOD}/" + "shapes3D/" + model + ".wrl"
+                    line_split_split = line_split[1].split()
+                    line_split_split[0] = model_path
+                    line_split[1] = " ".join(line_split_split)
+                    line_new = "(model".join(line_split) + "\n"
+        out_file.append(line_new)
+    pass
+    # write the file
+
+
 def main():
-    board = pcbnew.LoadBoard('En_mostic_test.kicad_pcb', True)
-    archive_symbols(board)
+    board = pcbnew.LoadBoard('En_mostic_test.kicad_pcb')
+
+    archive_3D_models(board)
+
+    archive_symbols(board, True)
+
+    # kot only kicad related
+    # os.getenv("KISYSMOD")
+    # os.getenv("KISYS3DMOD")
+    # os.getenv("KICAD_SYMBOL_DIR")
+    # os.getenv("KIPRJMOD")
+
 
 # for testing purposes only
 if __name__ == "__main__":
