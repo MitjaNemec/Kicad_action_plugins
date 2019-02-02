8550ea19cede8145e969491bf611e9a525e4d542
Mitja Nemec <mitja.nemec@fe.uni-lj.si>
2019-02-02 11:45:48 +0100

    Add additional layer of error reporting to catch and log errors when trying to run the plugin fails

--- replicate_layout/__init__.py
+++ replicate_layout/__init__.py
@@ -2,7 +2,9 @@ try:
     import wx
     from .action_replicate_layout import ReplicateLayout  # Note the relative import!
     ReplicateLayout().register()  # Instantiate and register to Pcbnew
-except:
+except Exception as e:
+    with open('Replicate_layout_error.log', 'w') as f:
+        f.write(repr(e))
     from .no_wxpython import NoWxpython as ReplicateLayout
     ReplicateLayout().register()  # Instantiate and register to Pcbnew
 
