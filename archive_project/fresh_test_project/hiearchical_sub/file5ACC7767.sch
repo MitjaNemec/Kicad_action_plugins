458a0f89b9f527f90632a6277aba05ea59d1a38c
Mitja Nemec <mitja.nemec@fe.uni-lj.si>
2019-03-12 21:54:25 +0100

    refreshed test suite

--- archive_project/fresh_test_project/hiearchical_sub/file5ACC7767.sch
+++ archive_project/fresh_test_project/hiearchical_sub/file5ACC7767.sch
@@ -1,10 +1,10 @@
 EESchema Schematic File Version 4
 LIBS:archive_test_project-cache
-EELAYER 26 0
+EELAYER 29 0
 EELAYER END
 $Descr A4 11693 8268
 encoding utf-8
-Sheet 1 4
+Sheet 4 4
 Title ""
 Date ""
 Rev ""
---
+++ archive_project/old_version.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#  action_replicate_layout.py
+#
+# Copyright (C) 2018 Mitja Nemec
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+import pcbnew
+import wx
+
+
+class OldVersion(pcbnew.ActionPlugin):
+    """
+    Notify user of missing wxpython
+    """
+    def defaults(self):
+        self.name = "Archive project"
+        self.category = "Archive project"
+        self.description = "Archive schematics symbols and 3D models"
+
+    def Run(self):
+        _pcbnew_frame = \
+            filter(lambda w: w.GetTitle().lower().startswith('pcbnew'),
+                   wx.GetTopLevelWindows()
+                   )[0]
+        caption = 'Archive project'
+        message = "This plugin works with KiCad 5.1 and higher"
+        dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_INFORMATION)
+        dlg.ShowModal()
+        dlg.Destroy()
