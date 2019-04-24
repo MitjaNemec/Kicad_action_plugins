MitjaNemec/Kicad_action_plugins

commit bac1ba0fc9092aa6687c81e767cc5032c6f5a158

refactor pa2pad to generated GUI code

pad2pad_track_distance/action_pad2pad_track_distance.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #  action_pad2pad_track_distance.py
 #
 # Copyright (C) 2018 Mitja Nemec
@@ -27,8 +28,10 @@ import sys
 
 if __name__ == '__main__':
     import pad2pad_track_distance
+    import pad2pad_track_distance_GUI
 else:
     from . import pad2pad_track_distance
+    from . import pad2pad_track_distance_GUI
 
 SCALE = 1000000.0
 
@@ -37,6 +40,32 @@ version_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "ve
 with open(version_filename) as f:
     VERSION = f.readline().strip()
 
+    
+class Pad2PadTrackDistanceDialog(pad2pad_track_distance_GUI.Pad2PadTrackDistanceGUI):
+    # hack for new wxFormBuilder generating code incompatible with old wxPython
+    # noinspection PyMethodOverriding
+    def SetSizeHints(self, sz1, sz2):
+        try:
+            # wxPython 3
+            self.SetSizeHintsSz(sz1, sz2)
+        except TypeError:
+            # wxPython 4
+            super(ArchiveProjectDialog, self).SetSizeHints(sz1, sz2)
+    
+    def __init__(self, parent, all_tracks, selected_tracks):
+        pad2pad_track_distance_GUI.Pad2PadTrackDistanceGUI.__init__(self, parent)
+        self.Fit()
+        self.all_tracks = all_tracks
+        self.selected_tracks = selected_tracks
+    
+    def highlight_tracks(self, event):
+        for track in self.all_tracks:
+            if track not in self.selected_tracks:
+                track.ClearBrightened()
+            else:
+                track.SetBrightened()
+        pcbnew.Refresh()
+    
 
 class Pad2PadTrackDistance(pcbnew.ActionPlugin):
     """
@@ -148,44 +177,17 @@ class Pad2PadTrackDistance(pcbnew.ActionPlugin):
         # deselect all tracks except used ones
         all_tracks = board.GetTracks()
 
-        for track in all_tracks:
-            if track not in measure_distance.track_list:
-                track.ClearSelected()
-            else:
-                track.SetSelected()
-                track.SetBrightened()
-                track.SetHighlighted()
+        dlg = Pad2PadTrackDistanceDialog(_pcbnew_frame, all_tracks, measure_distance.track_list)
 
-        # zoom the layout window to show the tracks taken into account
-        pad1_pos = selected_pads[0].GetPosition()
-        pad2_pos = selected_pads[1].GetPosition()
-        if pad1_pos[0] > pad2_pos[0]:
-            x = pad2_pos[0]
-            width = pad1_pos[0] - pad2_pos[0]
-        else:
-            x = pad1_pos[0]
-            width = pad2_pos[0] - pad1_pos[0]
-
-        if pad1_pos[1] > pad2_pos[1]:
-            y = pad2_pos[1]
-            height = pad1_pos[1] - pad2_pos[1]
-        else:
-            y = pad1_pos[1]
-            height = pad2_pos[1] - pad1_pos[1]
-
-        pcbnew.WindowZoom(x, y, width, height)
-        # pcbnew.Refresh()
-
-        caption = 'Pad2Pad Track Distance'
         if user_units == 'mm':
-            message = "Distance between pads is " + "%.3f" % (distance) + " mm" \
-                    + "\nResistance between pads is " + "%.4f" % resistance + " Ohm"
+            dlg.lbl_length.SetLabelText("%.3f" % (distance) + " mm")
+            dlg.lbl_resistance.SetLabelText("%.4f" % resistance + " Ohm")
         else:
-            message = "Distance between pads is " + "%.4f" % (distance/25.4) + " in" \
-                    + "\nResistance between pads is " + "%.4f" % resistance + " Ohm"
-        dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_INFORMATION)
-        dlg.ShowModal()
-        dlg.Destroy()
+            dlg.lbl_length.SetLabelText("%.4f" % (distance/25.4) + " in")
+            dlg.lbl_resistance.SetLabelText("%.4f" % resistance + " Ohm")
+
+        dlg.Show()
+
 
 class StreamToLogger(object):
     """

pad2pad_track_distance/pad2pad_track_distance.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #  pad2pad_track_distance.py
 #
 # Copyright (C) 2018 Mitja Nemec

pad2pad_track_distance/pad2pad_track_distance_GUI.py
@@ -1,0 +1,77 @@
+# -*- coding: utf-8 -*- 
+
+###########################################################################
+## Python code generated with wxFormBuilder (version Nov  6 2017)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO *NOT* EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+
+###########################################################################
+## Class Pad2PadTrackDistanceGUI
+###########################################################################
+
+class Pad2PadTrackDistanceGUI ( wx.Dialog ):
+	
+	def __init__( self, parent ):
+		wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"Pad2pad track distance", pos = wx.DefaultPosition, size = wx.Size( 269,135 ), style = wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER )
+		
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+		
+		bSizer4 = wx.BoxSizer( wx.VERTICAL )
+		
+		fgSizer1 = wx.FlexGridSizer( 2, 2, 0, 0 )
+		fgSizer1.SetFlexibleDirection( wx.BOTH )
+		fgSizer1.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+		
+		self.m_staticText2 = wx.StaticText( self, wx.ID_ANY, u"Distance between pads is:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText2.Wrap( -1 )
+		fgSizer1.Add( self.m_staticText2, 0, wx.ALL, 5 )
+		
+		self.lbl_length = wx.StaticText( self, wx.ID_ANY, u"0.0 mm", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.lbl_length.Wrap( -1 )
+		fgSizer1.Add( self.lbl_length, 0, wx.ALL, 5 )
+		
+		self.m_staticText3 = wx.StaticText( self, wx.ID_ANY, u"Resistance between pads is:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText3.Wrap( -1 )
+		fgSizer1.Add( self.m_staticText3, 0, wx.ALL, 5 )
+		
+		self.lbl_resistance = wx.StaticText( self, wx.ID_ANY, u"0.0 mΩ", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.lbl_resistance.Wrap( -1 )
+		fgSizer1.Add( self.lbl_resistance, 0, wx.ALL, 5 )
+		
+		
+		bSizer4.Add( fgSizer1, 1, wx.EXPAND, 5 )
+		
+		bSizer5 = wx.BoxSizer( wx.HORIZONTAL )
+		
+		self.btn_highlight = wx.Button( self, wx.ID_ANY, u"Highlight", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer5.Add( self.btn_highlight, 0, wx.ALL, 5 )
+		
+		self.btn_ok = wx.Button( self, wx.ID_OK, u"OK", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer5.Add( self.btn_ok, 0, wx.ALL, 5 )
+		
+		
+		bSizer4.Add( bSizer5, 1, wx.EXPAND, 5 )
+		
+		
+		self.SetSizer( bSizer4 )
+		self.Layout()
+		
+		self.Centre( wx.BOTH )
+		
+		# Connect Events
+		self.btn_highlight.Bind( wx.EVT_BUTTON, self.highlight_tracks )
+	
+	def __del__( self ):
+		pass
+	
+	
+	# Virtual event handlers, overide them in your derived class
+	def highlight_tracks( self, event ):
+		event.Skip()
+	
+

commit 79e56aeb5062b556be5ab169d0c97b6eee7f586d

Bugfix: pad2pad, copy/paste bug introduced in bac1ba0fc9092aa6687c81e767cc5032c6f5a158

pad2pad_track_distance/action_pad2pad_track_distance.py
@@ -50,14 +50,14 @@ class Pad2PadTrackDistanceDialog(pad2pad_track_distance_GUI.Pad2PadTrackDistance
             self.SetSizeHintsSz(sz1, sz2)
         except TypeError:
             # wxPython 4
-            super(ArchiveProjectDialog, self).SetSizeHints(sz1, sz2)
-    
+            super(Pad2PadTrackDistanceDialog, self).SetSizeHints(sz1, sz2)
+
     def __init__(self, parent, all_tracks, selected_tracks):
         pad2pad_track_distance_GUI.Pad2PadTrackDistanceGUI.__init__(self, parent)
         self.Fit()
         self.all_tracks = all_tracks
         self.selected_tracks = selected_tracks
-    
+
     def highlight_tracks(self, event):
         for track in self.all_tracks:
             if track not in self.selected_tracks:
@@ -65,7 +65,7 @@ class Pad2PadTrackDistanceDialog(pad2pad_track_distance_GUI.Pad2PadTrackDistance
             else:
                 track.SetBrightened()
         pcbnew.Refresh()
-    
+
 
 class Pad2PadTrackDistance(pcbnew.ActionPlugin):
     """