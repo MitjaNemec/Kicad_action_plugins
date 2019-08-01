MitjaNemec/Kicad_action_plugins

commit 6c19fac3acdce9df17ce5b648e665e8173e4ac8b

Added logging.shutdown to flush the log

archive_project/action_archive_project.py
@@ -112,6 +112,7 @@ class ArchiveProject(pcbnew.ActionPlugin):
             dlg.ShowModal()
             dlg.Destroy()
             logger.info("Exiting as eeschema is opened")
+            logging.shutdown()
             return
 
         # show dialog
@@ -131,6 +132,7 @@ class ArchiveProject(pcbnew.ActionPlugin):
         # exit the plugin
         else:
             logger.info("Action plugin canceled on first dialog")
+            logging.shutdown()
             return
 
         if main_dialog.chkbox_sch.GetValue():
@@ -156,16 +158,26 @@ class ArchiveProject(pcbnew.ActionPlugin):
                     logger.info("Retrying schematics archiving")
                     archive_project.archive_symbols(board, allow_missing_libraries=True, alt_files=False)
                 else:
+                    logging.shutdown()
                     return
             except Exception:
                 logger.exception("Fatal error when archiveing schematics")
-                raise
+                caption = 'Archive project'
+                message = "Fatal error when archiveing schematics.\n"\
+                        + "You can raise an issue on GiHub page.\n" \
+                        + "Please attach the net2et_distance.log which you should find in the project folder."
+                dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_ERROR)
+                dlg.ShowModal()
+                dlg.Destroy()
+                logging.shutdown()
+                return
 
             caption = 'Archive project'
             message = "Schematics archived sucessfuly!"
             dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_EXCLAMATION)
             dlg.ShowModal()
             dlg.Destroy()
+            logging.shutdown()
 
         if main_dialog.chkbox_3D.GetValue():
             try:
@@ -182,6 +194,7 @@ class ArchiveProject(pcbnew.ActionPlugin):
                     logger.info("Retrying 3D model archiving")
                     archive_project.archive_3D_models(board, allow_missing_models=True, alt_files=False)
                 else:
+                    logging.shutdown()
                     return
 
             caption = 'Archive project'
@@ -190,6 +203,7 @@ class ArchiveProject(pcbnew.ActionPlugin):
             dlg.ShowModal()
             dlg.Destroy()
             main_dialog.Destroy()
+            logging.shutdown()
 
 
 class StreamToLogger(object):

length_stats/action_length_stats.py
@@ -104,7 +104,7 @@ class LenghtStatsDialog(lenght_stats_GUI.LenghtStatsGUI):
         pcbnew.Refresh()
 
         self.logger.info("Closing GUI")
-
+        logging.shutdown()
         self.Close()
         event.Skip()
 

net2net_min_distance/action_net2net_min_distance.py
@@ -65,7 +65,7 @@ class Net2NedDistance(pcbnew.ActionPlugin):
 
         # set up logger
         logging.basicConfig(level=logging.DEBUG,
-                            filename="net2et distance.log",
+                            filename="net2et_distance.log",
                             filemode='w',
                             format='%(asctime)s %(name)s %(lineno)d:%(message)s',
                             datefmt='%m-%d %H:%M:%S')
@@ -107,8 +107,16 @@ class Net2NedDistance(pcbnew.ActionPlugin):
             dis, loc = net2net_distance.get_min_distance(board, list(nets))
         except Exception:
             logger.exception("Fatal error when replicating")
-            raise
-
+            caption = 'Net2Net Track Distance'
+            message = "Fatal error when replicating.\n"\
+                    + "You can raise an issue on GiHub page.\n" \
+                    + "Please attach the net2et_distance.log which you should find in the project folder."
+            dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_ERROR)
+            dlg.ShowModal()
+            dlg.Destroy()
+            logging.shutdown()
+            return
+            
         caption = 'Net2Net Track Distance'
         if user_units == 'mm':
             message = "Minimum distance between net segments is " + "%.3f" % (dis/SCALE) + " mm"
@@ -117,6 +125,7 @@ class Net2NedDistance(pcbnew.ActionPlugin):
         dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_INFORMATION)
         dlg.ShowModal()
         dlg.Destroy()
+        logging.shutdown()
 
 
 class StreamToLogger(object):

pad2pad_track_distance/action_pad2pad_track_distance.py
@@ -71,6 +71,7 @@ class Pad2PadTrackDistanceDialog(pad2pad_track_distance_GUI.Pad2PadTrackDistance
         for track in self.selected_tracks:
             track.ClearBrightened()
         pcbnew.Refresh()
+        logging.shutdown()
         event.Skip()
 
 
@@ -149,6 +150,7 @@ class Pad2PadTrackDistance(pcbnew.ActionPlugin):
             dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_INFORMATION)
             dlg.ShowModal()
             dlg.Destroy()
+            logging.shutdown()
             return
 
         # and if they are on different nest
@@ -158,6 +160,7 @@ class Pad2PadTrackDistance(pcbnew.ActionPlugin):
             dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_INFORMATION)
             dlg.ShowModal()
             dlg.Destroy()
+            logging.shutdown()
             return
 
         try:
@@ -170,10 +173,19 @@ class Pad2PadTrackDistance(pcbnew.ActionPlugin):
             dlg.ShowModal()
             dlg.Destroy()
             logger.info(message)
+            logging.shutdown()
             return
         except Exception:
             logger.exception("Fatal error when measuring")
-            raise
+            caption = 'Pad2Pad Track Distance'
+            message = "Fatal error when measuring.\n"\
+                    + "You can raise an issue on GiHub page.\n" \
+                    + "Please attach the pad2pad_track_distance.log which you should find in the project folder."
+            dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_ERROR)
+            dlg.ShowModal()
+            dlg.Destroy()
+            logging.shutdown()
+            return
 
         # trying to show in layout which tracks are taken into account - so far it does not work
         # as the selection is automatically cleared when exiting action plugin
@@ -213,4 +225,3 @@ class StreamToLogger(object):
     def flush(self, *args, **kwargs):
         """No-op for wrapper"""
         pass
-

place_footprints/action_place_footprints.py
@@ -572,8 +572,8 @@ class PlaceFootprints(pcbnew.ActionPlugin):
                     logger.info("Placing complete")
                     logging.shutdown()
                 except Exception:
-                    logger.exception("Fatal error when executing place footprints")
                     caption = 'Place footprints'
+                    logger.exception("Fatal error when executing place footprints")
                     message = "Fatal error when executing place footprints.\n"\
                             + "You can raise an issue on GiHub page.\n" \
                             + "Please attach the place_footprints.log which you should find in the project folder."

save_restore_layout/action_save_restore_layout.py
@@ -63,7 +63,7 @@ class SaveDialog(save_layout_dialog_GUI.SaveLayoutDialogGUI):
 
     def __init__(self, parent, levels):
         save_layout_dialog_GUI.SaveLayoutDialogGUI.__init__(self, parent)
-        
+
         # clear levels
         self.list_levels.Clear()
         self.list_levels.AppendItems(levels)
@@ -126,6 +126,7 @@ class SaveRestoreLayout(pcbnew.ActionPlugin):
             dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_INFORMATION)
             dlg.ShowModal()
             dlg.Destroy()
+            logging.shutdown()
             return
 
         # this it the reference footprint
@@ -151,6 +152,7 @@ class SaveRestoreLayout(pcbnew.ActionPlugin):
                 dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_ERROR)
                 dlg.ShowModal()
                 dlg.Destroy()
+                logging.shutdown()
                 return
 
             # find out the available hierarchical levels
@@ -161,6 +163,7 @@ class SaveRestoreLayout(pcbnew.ActionPlugin):
             level_dialog = SaveDialog(_pcbnew_frame, levels)
             res = level_dialog.ShowModal()
             if res != wx.ID_OK:
+                logging.shutdown()
                 return
             index = level_dialog.list_levels.GetSelection()
             level_dialog.Destroy()
@@ -171,6 +174,7 @@ class SaveRestoreLayout(pcbnew.ActionPlugin):
                 dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_INFORMATION)
                 dlg.ShowModal()
                 dlg.Destroy()
+                logging.shutdown()
                 return
 
             # Once user selects a level ask the user top specify file
@@ -178,6 +182,7 @@ class SaveRestoreLayout(pcbnew.ActionPlugin):
             dlg = wx.FileDialog(_pcbnew_frame, "Select a file", os.getcwd(), "", wildcard, wx.SAVE)
             res = dlg.ShowModal()
             if res != wx.ID_OK:
+                logging.shutdown()
                 return
             layout_file = dlg.GetPath()
             dlg.Destroy()
@@ -193,6 +198,7 @@ class SaveRestoreLayout(pcbnew.ActionPlugin):
                 dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_ERROR)
                 dlg.ShowModal()
                 dlg.Destroy()
+                logging.shutdown()
                 return
 
         # restore layout
@@ -204,6 +210,7 @@ class SaveRestoreLayout(pcbnew.ActionPlugin):
             dlg = wx.FileDialog(_pcbnew_frame, "Choose a file", os.getcwd(), "", wildcard, wx.OPEN)
             res = dlg.ShowModal()
             if res != wx.ID_OK:
+                logging.shutdown()
                 return
             layout_file = dlg.GetPath()
             dlg.Destroy()
@@ -220,6 +227,7 @@ class SaveRestoreLayout(pcbnew.ActionPlugin):
                 dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_ERROR)
                 dlg.ShowModal()
                 dlg.Destroy()
+                logging.shutdown()
                 return
 
             pivot_mod = restore_layout.get_mod_by_ref(pivot_module_reference)
@@ -235,6 +243,7 @@ class SaveRestoreLayout(pcbnew.ActionPlugin):
                 dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_ERROR)
                 dlg.ShowModal()
                 dlg.Destroy()
+                logging.shutdown()
                 return
 
 

swap_pins/action_swap_pins.py
@@ -91,6 +91,7 @@ class SwapPins(pcbnew.ActionPlugin):
             dlg.ShowModal()
             dlg.Destroy()
             logger.info("Action plugin canceled as eeschema was not closed")
+            logging.shutdown()
             return
 
         # check if there are precisely two pads selected
@@ -102,6 +103,7 @@ class SwapPins(pcbnew.ActionPlugin):
             dlg.ShowModal()
             dlg.Destroy()
             logger.info("Action plugin canceled. More or less than 2 pads selected.")
+            logging.shutdown()
             return
 
         # are they on the same module
@@ -114,11 +116,13 @@ class SwapPins(pcbnew.ActionPlugin):
             dlg.ShowModal()
             dlg.Destroy()
             logger.info("Action plugin canceled. Selected pads don't belong to the same footprint.")
+            logging.shutdown()
             return
 
         # swap pins
         try:
             swap_pins.swap(board, pad1, pad2)
+            logging.shutdown()
         except (ValueError, LookupError) as error:
             caption = 'Swap pins'
             message = str(error)
@@ -126,9 +130,19 @@ class SwapPins(pcbnew.ActionPlugin):
             dlg.ShowModal()
             dlg.Destroy()
             logger.exception("Gracefully handled error while running")
+            logging.shutdown()
         except Exception:
-            logger.exception("Fatal error when swapping units")
-            raise
+            logger.exception("Fatal error when swapping pins")
+            caption = 'Swap pins'
+            message = "Fatal error when swapping pins.\n"\
+                    + "You can raise an issue on GiHub page.\n" \
+                    + "Please attach the swap_pins.log which you should find in the project folder."
+            dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_ERROR)
+            dlg.ShowModal()
+            dlg.Destroy()
+            logging.shutdown()
+            logging.shutdown()
+            return
 
 
 class StreamToLogger(object):

swap_units/action_swap_units.py
@@ -91,6 +91,7 @@ class SwapUnits(pcbnew.ActionPlugin):
             dlg.ShowModal()
             dlg.Destroy()
             logger.info("Action plugin canceled as eeschema was not closed")
+            logging.shutdown()
             return
 
         # load board
@@ -105,6 +106,7 @@ class SwapUnits(pcbnew.ActionPlugin):
             dlg.ShowModal()
             dlg.Destroy()
             logger.info("Action plugin canceled. More or less than 2 pads selected.")
+            logging.shutdown()
             return
 
         # are they on the same module
@@ -117,11 +119,13 @@ class SwapUnits(pcbnew.ActionPlugin):
             dlg.ShowModal()
             dlg.Destroy()
             logger.info("Action plugin canceled. Selected pads don't belong to the same footprint.")
+            logging.shutdown()
             return
 
         # swap pins
         try:
             swap_units.swap(board, pad1, pad2)
+            logging.shutdown()
         except LookupError as error:
             caption = 'Swap units'
             message = str(error)
@@ -129,9 +133,18 @@ class SwapUnits(pcbnew.ActionPlugin):
             dlg.ShowModal()
             dlg.Destroy()
             logger.exception("Gracefully hnadled error while running")
+            logging.shutdown()
         except Exception:
             logger.exception("Fatal error when swapping units")
-            raise
+            caption = 'Swap units'
+            message = "Fatal error when swapping units.\n"\
+                    + "You can raise an issue on GiHub page.\n" \
+                    + "Please attach the swap_units.log which you should find in the project folder."
+            dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_ERROR)
+            dlg.ShowModal()
+            dlg.Destroy()
+            logging.shutdown()
+            return
 
 
 class StreamToLogger(object):

commit 7baaa5736e1c1ecf3140452c378da07060238538

fix a bug introduced in 6c19fac3acdce9df17ce5b648e665e8173e4ac8b

archive_project/action_archive_project.py
@@ -150,10 +150,11 @@ class ArchiveProject(pcbnew.ActionPlugin):
         if main_dialog.chkbox_sch.GetValue():
             # archive schematics
             try:
-                logger.info("Starting schematics archiving")
                 if main_dialog.chkbox_pdf.GetValue():
+                    logger.info("Starting archiving schematics and documentation files")
                     archive_project.archive_symbols(board, allow_missing_libraries=False, alt_files=False, archive_documentation=True)
                 else:
+                    logger.info("Starting archiving only schematics")
                     archive_project.archive_symbols(board, allow_missing_libraries=False, alt_files=False, archive_documentation=False)
             except (ValueError, IOError, LookupError) as error:
                 caption = 'Archive project'
@@ -195,7 +196,6 @@ class ArchiveProject(pcbnew.ActionPlugin):
             dlg = wx.MessageDialog(_pcbnew_frame, message, caption, wx.OK | wx.ICON_EXCLAMATION)
             dlg.ShowModal()
             dlg.Destroy()
-            logging.shutdown()
 
         if main_dialog.chkbox_3D.GetValue():
             try:

archive_project/archive_project.py
@@ -594,6 +594,7 @@ def main():
         print(str(error))
     saved = pcbnew.SaveBoard(board.GetFileName().replace(".kicad_pcb", "_temp.kicad_pcb"), board)
 
+
 # for testing purposes only
 if __name__ == "__main__":
     # if debugging outside of this folder change the folder